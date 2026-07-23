42293185146c7d56b145adbc4e7db544fd6eb120
Slayxer <YOUR_GITHUB_EMAIL>
2026-07-23 17:50:23 +0530

    Revert deployment configuration

--- client/src/socket/socket.js
+++ client/src/socket/socket.js
@@ -1,16 +1,16 @@
 import { io } from "socket.io-client";
 
-const socket = io(import.meta.env.VITE_SOCKET_URL, {
-    autoConnect: false,
-    transports: ["websocket"]
+const socket = io("http://localhost:5000", {
+  autoConnect: false,
+  transports: ["websocket"],
 });
 
 socket.on("connect", () => {
-    console.log("🟢 Socket Connected:", socket.id);
+  console.log("🟢 Socket Connected:", socket.id);
 });
 
 socket.on("disconnect", () => {
-    console.log("🔴 Socket Disconnected");
+  console.log("🔴 Socket Disconnected");
 });
 
 export default socket;
